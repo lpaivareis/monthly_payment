5934b579f44d8f5c86c4ab9e9299adf21a9d5a6e
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-24 21:41:53 -0300

    fix rubocop offenses

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,8 @@ class Student < ApplicationRecord
   validate :name, presence: true
   validate :document, unique: true
 
-  enum payment_method:{
+  enum payment_method: {
     credit_card: "credit_card",
     boleto: "boleto"
   }
-
-
 end
