c9955102e9bade13f1da8db0d19782815f224851
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-24 21:33:59 -0300

    add student model

---
+++ app/models/student.rb
@@ -0,0 +1,11 @@
+class Student < ApplicationRecord
+  validate :name, presence: true
+  validate :document, unique: true
+
+  enum payment_method:{
+    credit_card: "credit_card",
+    boleto: "boleto"
+  }
+
+
+end
