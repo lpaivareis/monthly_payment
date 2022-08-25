792534bd8abba3e0708bd0f42d02ec8362d9a287
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-25 17:30:27 -0300

    add enrollment controller

--- app/controllers/api/enrollments_controller.rb
+++ app/controllers/api/enrollments_controller.rb
@@ -1,13 +1,53 @@
-module API
+module Api
   class EnrollmentsController < ApplicationController
-    def index; end
+    before_action :set_enrollment, only: %i[update show destroy]
+    after_action :create_bills, only: %i[create update]
 
-    def create; end
+    def index
+      @enrollments = Enrollment.all.limit(params[:count])
+      render json: { page: params[:page], items: @enrollments }.to_json, status: :ok
+    end
 
-    def show; end
+    def create
+      @enrollment = Enrollment.new(enrollment_params)
 
-    def update; end
+      if @enrollment.save
+        render json: { id: @enrollment.id }.to_json, status: :created
+      else
+        render json: @enrollment.errors.to_json, status: :unprocessable_entity
+      end
+    end
 
-    def destroy; end
+    def show
+      render json: @enrollment.to_json, status: :ok
+    end
+
+    def update
+      if @enrollment.update(enrollment_params)
+        render json: { message: "Matricula atualizada com sucesso!" }.to_json, status: :ok
+      else
+        render json: @enrollment.errors.to_json, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      if @enrollment.destroy
+        render json: { message: "Matricula removida com sucesso!" }.to_json, status: :ok
+      else
+        render json: @enrollment.errors.to_json, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def enrollment_params
+      params.permit(:amount, :installments, :due_day)
+    end
+
+    def set_enrollment
+      @enrollment = Enrollment.find(params[:id])
+    end
+
+    def create_bills; end
   end
 end
