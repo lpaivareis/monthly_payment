7dcc3d27757d40e3dc76558bdf4dc62156db923a
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-25 17:30:19 -0300

    add students controller

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,13 +1,50 @@
-module API
+module Api
   class StudentsController < ApplicationController
-    def index; end
+    before_action :set_student, only: %i[update show destroy]
 
-    def create; end
+    def index
+      @students = Student.all.limit(params[:count])
+      render json: { page: params[:page], items: @students }.to_json, status: :ok
+    end
 
-    def show; end
+    def create
+      @student = Student.new(student_params)
 
-    def update; end
+      if @student.save
+        render json: { id: @student.id }.to_json, status: :created
+      else
+        render json: @student.errors.to_json, status: :unprocessable_entity
+      end
+    end
 
-    def destroy; end
+    def show
+      render json: @student.to_json, status: :ok
+    end
+
+    def update
+      if @student.update(student_params)
+        render json: { message: "Estudante atualizado com sucesso!" }.to_json, status: :ok
+      else
+        render json: @student.errors.to_json, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      if @student.destroy
+        render json: { message: "Estudante removido com sucesso!" }.to_json, status: :ok
+      else
+        render json: @student.errors.to_json, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def student_params
+      params.permit(:name, :payment_method, :birthdate, :cpf)
+    end
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
   end
 end
