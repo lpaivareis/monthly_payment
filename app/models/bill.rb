bb6a9d0730adaa2901691b56e57a333d233e2148
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-25 15:23:50 -0300

    fix models

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,7 +3,7 @@ class Bill < ApplicationRecord
             :due_date,
             :status, presence: true
 
-  validates :amount, numericality: { greater_than: 0 }
+  validates_numericality_of :amount, greater_than: 0, message: "Deve ser maior que 0!"
 
   enum status: {
     open: "open",
