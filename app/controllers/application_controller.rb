ca6fd1deb3e2fff0f06ffa24e7ce2bffc2561ddb
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-08-25 23:11:10 -0300

    add basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  before_action :authentication
+
+  protected
+  
+  def authentication
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin_ops" && password == "billing"
+    end
+  end
 end
